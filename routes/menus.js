6bedfd4ada5c057875a743b9cef5c92fe2440f97
rain <3100406687@qq.com>
2022-05-12 21:22:57 +0800

    按钮结构树

--- routes/menus.js
+++ routes/menus.js
@@ -5,6 +5,48 @@ const Menu = require('../model/menuSchema') // 引入模型
 
 router.prefix('/menu') // 设置路由前缀
 
+router.get('/list', async (ctx) => {
+  const {
+    menuName,
+    menuState,
+  } = ctx.request.body
+
+  const params = {}
+
+  if (menuName) params.menuName = menuName
+  if (menuState) params.menuState = menuState
+
+  let rootList = await Menu.find(params) || []
+
+  const permissionList = getTreeMenu(rootList, null, [])
+  ctx.body = util.success(permissionList)
+})
+
+// 递归拼接属性列表
+function getTreeMenu(rootList, id, list) {
+  for (let i = 0; i < rootList.length; i++) {
+    let item = rootList[i]
+    if (String(item.parentId.slice().pop()) == String(id)) {
+      console.log(item.parentId.slice().pop())
+      list.push(item._doc)
+    }
+  }
+  list.map(item => {
+    item.children = []
+    getTreeMenu(rootList, item._id, item.children)
+    if (item.children.length == 0) {
+      delete item.children
+    } else if (item.children.length > 0 && item.children[0].menuType == 2) { // 2 是按钮
+      // 快速区分按钮和菜单，用于后期做菜单按钮权限控制
+      item.action = item.children
+    }
+  })
+  return list
+}
+
+/**
+ * 按钮的添加、删除、编辑
+ */
 router.post('/operate', async (ctx) => {
   // 解析出_id和action，剩下的一起解析
   const {
